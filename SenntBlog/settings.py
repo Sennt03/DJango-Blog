ea4e75f49424a45493f6f6bba702b05cb56d247f
David <davidrsmk144@gmail.com>
2021-05-20 17:08:29 -0500

    fsd

--- SenntBlog/settings.py
+++ SenntBlog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '*$o(z#s6ueulr=&x^zqqq^9h7s)ukw8tpfam-f%j!=3g_75s+g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
